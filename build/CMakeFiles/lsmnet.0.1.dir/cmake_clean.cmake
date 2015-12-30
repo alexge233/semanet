57435a3fb36d9ad368e28cc9c0f6e9c0edc12962
alex <alex@athena.utopia>
2015-12-30 11:47:13 +0000

    start

--- build/CMakeFiles/lsmnet.0.1.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblsmnet.0.1.pdb"
-  "liblsmnet.0.1.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/lsmnet.0.1.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
